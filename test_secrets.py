83f6b77232d58eebd287e3c1bd39dcd310214ff5
copchase <lscnr.thc@gmail.com>
2021-08-22 11:29:57 -0700

    Add secret update tests

--- test_secrets.py
+++ test_secrets.py
@@ -24,3 +24,24 @@ def test_get_secret(mocker):
     result = secrets.get_secret("my-secret")
 
     assert result == "hello world"
+
+
+def test_update_secret_exception(mocker):
+    mock_error = ClientError({"Error": {"Code": "ErrorCode"}}, "test_update_secret_exception")
+    mock_secrets_client = MagicMock()
+    mock_secrets_client.configure_mock(**{"update_secret.side_effect": mock_error})
+    mocker.patch("boto3.client", return_value=mock_secrets_client)
+
+    result = secrets.update_secret("my-secret", "new-secret")
+
+    assert result is False
+
+
+def test_update_secret(mocker):
+    mock_secrets_client = MagicMock()
+    mock_secrets_client.configure_mock(**{"update_secret.return_value": None})
+    mocker.patch("boto3.client", return_value=mock_secrets_client)
+
+    result = secrets.update_secret("my-secret", "new-secret")
+
+    assert result is True
