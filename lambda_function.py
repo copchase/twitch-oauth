f5375688349bd3ae77a64a596ac0892b4717e7ee
copchase <lscnr.thc@gmail.com>
2021-08-22 19:54:10 -0700

    Add exception log

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,11 @@
+from logzero import logger
+
 import tokens
 
 
 def lambda_handler(event: dict, context) -> str:
     try:
         return tokens.get_valid_token()
-    except Exception:
+    except Exception as e:
+        logger.exception(e)
         return None
